e3d7ceb33f82496e0b11e1e687c040bd57f5e4ac
Jeff Miranda <jeffmiranda@Jeff-Mirandas-New-MacBook-Pro.local>
2015-10-23 16:26:42 -0600

    Defined constants for the build script

--- onsightphoto/_build/build.config.php
+++ onsightphoto/_build/build.config.php
@@ -4,7 +4,11 @@ define('MODX_CORE_PATH', MODX_BASE_PATH . 'core/');
 define('MODX_MANAGER_PATH', MODX_BASE_PATH . 'manager/');
 define('MODX_CONNECTORS_PATH', MODX_BASE_PATH . 'connectors/');
 define('MODX_ASSETS_PATH', MODX_BASE_PATH . 'assets/');
- 
+
+define('OSP_BASE_PATH', '/Users/jeffmiranda/Sites/onsightphoto/onsightphoto/');
+define('OSP_CORE_PATH', OSP_BASE_PATH . 'core/');
+define('OSP_ASSETS_PATH', OSP_BASE_PATH . 'assets/');
+
 define('MODX_BASE_URL','/');
 /* define('MODX_CORE_URL', MODX_BASE_URL . 'core/'); */ /* There isn't a core URL!! */
 define('MODX_MANAGER_URL', MODX_BASE_URL . 'manager/');
